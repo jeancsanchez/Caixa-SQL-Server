7b0e47f92f160bc8a35c6d52b916ca7942c24ecf
Jean Carlos <jeancs.patricio@gmail.com>
2015-03-28 01:43:24 -0300

    Sistema de caixa usando SQL Server e CSharp

---
+++ Simple Cash 1.0 with SQL/Forms/frmGerente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Cash_1._0_with_SQL
+{
+    public partial class frmGerente : Form
+    {
+        public frmGerente()
+        {
+            InitializeComponent();
+        }
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            if (txtLogiGerente.Text == "100" && txtSenhagerente.Text == "9494")
+            {
+                frmCadastroFuncionario y = new frmCadastroFuncionario();
+                y.Show();
+            }
+            else
+                MessageBox.Show("Somente o gerente pode fazer essa operação!");
+        }
+    }
+}
---
+++ Simple Cash 1.0 with SQL/Model/Funcionario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using Simple_Cash_1._0_with_SQL;
+
+namespace Simple_Cash_1._0_with_SQL.Model
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public void CadastrarProduto(Produto produto)
+        {
+            SqlConnection on = new SqlConnection(Conexao.StringConection);
+            SqlCommand cm = new SqlCommand("insert into produtos values(@Nome,@preco)", on);
+            cm.Parameters.AddWithValue("@nome", produto.Nome);
+            cm.Parameters.AddWithValue("@preco", produto.Preco);
+
+            try
+            {
+                on.Open();
+                cm.ExecuteNonQuery();
+                MessageBox.Show("Produto cadastrado com sucesso!");
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            finally
+            {
+                on.Close();
+            }
+        }
+    }
+}
